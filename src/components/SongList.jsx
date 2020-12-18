1d6f281493d7f9a6700f97e484eae50e48aeb799
zhaohonglee <296222105@qq.com>
2020-12-18 15:41:10 +0800

    hooks frist commit

---
+++ src/components/SongList.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid' //npm安装uuid 这个插件，用来随机生成不重复的id编码
+
+export default function SongList() {
+  // 1.useState是一个函数，返回两个值，返回一个原始数组和修改后的数组
+  const [songs, setSongs] = useState([
+    { title: 'Lemon', id: 1 },
+    { title: '打上花火', id: 2 },
+    { title: '青花瓷', id: 3 }
+  ])
+
+  const addSong = () => {
+    setSongs([...songs, { title: '成都', id: uuidv4() }])
+  }
+
+  return (
+    <div className='song-list' >
+      <ul>
+        {songs.map(item => {
+          return (
+            <li key={item.id}>{item.title}</li>
+          )
+        })}
+      </ul>
+      <button onClick={addSong}>添加歌曲</button>
+    </div>
+  )
+}
